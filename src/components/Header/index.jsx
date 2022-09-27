d1bd7a5bacb5fed6c9bec11c637fa0a4cee6276c
GarryProhor <igorprohorchenko@gmail.com>
2022-09-27 16:57:13 +0300

    header top

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Col, Container, Row} from "reactstrap";
+import {Link} from "react-router-dom";
+import './Header.css';
 
 const Header = () => {
     return (
@@ -16,7 +18,10 @@ const Header = () => {
                            </div>
                        </Col>
                        <Col lg='6' md='6' sm='6'>
-
+                            <div className='header__top-right d-flex align-items-center justify-content-end gap-3'>
+                                <Link to='#' className='d-flex align-items-center gap-1'><i className="ri-login-circle-line"></i>Login</Link>
+                                <Link to='#' className='d-flex align-items-center gap-1'><i className="ri-user-line"></i>Register</Link>
+                            </div>
                        </Col>
                    </Row>
                </Container>
